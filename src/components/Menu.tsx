8caa609b9dbb1641e7f7e7cf96e81103d0039e5d
John Gorriceta <johngorriceta@johns-air.mynetworksettings.com>
2023-12-31 23:59:21 -0700

    not italic

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,7 +6,7 @@ export default function Menu({ children}: Props){
     return(
         <div className="w-full h-screen px-5 pb-5 bg-black">
             <div className="bg-black flex justify-center items-center z-10 sticky top-0 h-[10%]">
-                <p className="text-4xl text-white italic font-black">MUNCHY <span className="font-hand">
+                <p className="text-4xl text-white italic font-black">MUNCHY <span className="font-hand not-italic">
                     <span className="text-pink-500">C</span>
                     <span className="text-purple-500">O</span>
                     <span className="text-green-300">L</span>
